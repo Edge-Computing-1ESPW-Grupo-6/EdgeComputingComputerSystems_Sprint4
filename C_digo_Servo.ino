65c36cf8b51c596db05029c18067f69acdfb490b
Vinicius Buzato <127258256+ViniciusBuzato@users.noreply.github.com>
2023-11-06 21:57:06 -0300

    Update C_digo_Servo.ino

--- C_digo_Servo.ino
+++ C_digo_Servo.ino
@@ -1,7 +1,7 @@
 #include "Arduino.h"
 #include "WiFi.h"
 #include "HTTPClient.h"
-#include <Servo.h>
+#include <ESP32Servo.h>
 
 #define servoPIN 26
 
@@ -24,74 +24,72 @@ long duration2;
 int distance2;
 
 int aberturas = 0;
+int porta = 0; // Initialize porta variable
 
-char ssid[] = "Vini";
+char ssid[] = "Vini"; 
 char pass[] = "12345678";
 char serverAddress[] = "https://api.tago.io/data";  // TagoIO address
 char contentHeader[] = "application/json";
-char tokenHeader[]   = "d2e67330-bc54-4e6a-902d-a7bf4e67846f"; // TagoIO Token
+char tokenHeader[] = "d2e67330-bc54-4e6a-902d-a7bf4e67846f"; // TagoIO Token
 
+HTTPClient client;  // Declare an instance of the HTTPClient class
 
-void setup() {​
-  // put your setup code here, to run once:
+void setup() {
+  // put your setup code here, to run once
   Serial.begin(9600);
   init_wifi();
 
   meuServo.attach(servoPIN);
-  meuServo.write(0); // Inicia motor posição zero
+
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  
-}​
-void init_wifi() {​
+}
+
+void init_wifi() {
   Serial.println("Conectando WiFi");
   WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED) {​
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(1000);
-  }​
+  }
   Serial.println("Conectado");
   Serial.print("Meu IP eh: ");
   Serial.println(WiFi.localIP());
-}​
+}
 
-void loop() {​
-
+void loop() {
   lerSerial();
-  
-  if(serialOrder == 1){
-    moverServo(90);  // Move o servo para 90 graus
-    if(previousOpen == 0){
-      previousOpen = 1;
-      aberturas++
-      enviarPortaAberta();
-    }
-    Serial.println("A porta esta aberta")
-  } else{
-    
-    lerSensor();
 
-    if (porta == 1) {
-      moverServo(90);  // Move o servo para 90 graus
-      if(previousOpen == 0){
+  if (serialOrder == 1) {
+    moverServo(90);  // Move the servo to 90 degrees
+    if (previousOpen == 0) {
       previousOpen = 1;
       aberturas++;
       enviarPortaAberta();
-      }
-      Serial.println("A porta esta aberta")
-    } 
-    else {
-      moverServo(0);  // Mantém o servo na posição 0 graus
-      previousOpen = 0
     }
-    
-  }
-}​
+    Serial.println("A porta esta aberta");
+  } else {
+    lerSensor();
 
-void lerSerial(){
+    if (porta == 1) {
+      moverServo(90);  // Move the servo to 90 degrees
+      if (previousOpen == 0) {
+        previousOpen = 1;
+        aberturas++;
+        enviarPortaAberta();
+      }
+      Serial.println("A porta esta aberta");
+    } else {
+      moverServo(0);  // Keep the servo at 0 degrees
+      previousOpen = 0;
+    }
+  }
+}
+
+void lerSerial() {
   if (Serial.available() > 0) {
     serialCommand = Serial.read();
-    // Verifique o comando e realize a ação desejada
+    // Check the command and perform the desired action
     if (serialCommand == 'O') {
       serialOrder = 1; // Open the door
     } else if (serialCommand == 'C') {
@@ -100,50 +98,38 @@ void lerSerial(){
   }
 }
 
-void lerSensor1(){
+void lerSensor() {
+  // Read Sensor 1
 
-    // LENDO GENRO 1
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.print("Distance: ");
+  Serial.println(distance);
 
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    // Sets the trigPin on HIGH state for 10 micro seconds
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    duration = pulseIn(echoPin, HIGH);
-    // Calculating the distance
-    distance = duration * 0.034 / 2;
-    // Prints the distance on the Serial Monitor
-    Serial.print("Distance: ");
-    Serial.println(distance);
+  // Read Sensor 2
 
-    // LENDO GENRO 2
+  digitalWrite(trigPin2, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin2, LOW);
+  duration2 = pulseIn(echoPin2, HIGH);
+  distance2 = duration2 * 0.034 / 2;
 
-    digitalWrite(trigPin2, LOW);
-    delayMicroseconds(2);
-    // Sets the trigPin on HIGH state for 10 micro seconds
-    digitalWrite(trigPin2, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin2, LOW);
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    duration2 = pulseIn(echoPin2, HIGH);
-    // Calculating the distance
-    distance2 = duration * 0.034 / 2;
-    // Prints the distance on the Serial Monitor
-    Serial.print("Distance 2: ");
-    Serial.println(distance2);
-
-  if(distance <= 5 || distance2 <= 5){
-    porta == 1;
-  }else{
-    porta == 0;
+  if (distance <= 10) {
+    porta = 1; // Set porta to 1 if either distance is less than or equal to 5
+  } else {
+    porta = 0; // Otherwise, set porta to 0
   }
- }
+}
 
 void moverServo(int angulo) {
-  meuServo.write(angulo);  // Move o servo para a posição especificada
-  delay(500);  // Aguarda um curto período para o servo atingir a posição desejada
+  meuServo.write(angulo);  // Move the servo to the specified position
 }
 
 void enviarPortaAberta() {
@@ -152,14 +138,18 @@ void enviarPortaAberta() {
   char anyData1[30];
   char bAny[30];
   int statusCode = 0;
+  
   strcpy(postData, "{\n\t\"variable\": \"Aberturas\",\n\t\"value\": ");
   dtostrf(aberturas, 6, 2, anyData);
+  strncat(postData, anyData, 100);
+  strcpy(anyData1, ",\n\t\"unit\": \"C\"\n\t}\n");
   strncat (postData, anyData1, 100);
   Serial.println(postData);
   client.begin(serverAddress);
   client.addHeader("Content-Type", contentHeader);
   client.addHeader("Device-Token", tokenHeader);
   statusCode = client.POST(postData);
+  delay (500);
   Serial.print("Status code: ");
   Serial.println(statusCode);
   Serial.println("End of POST to TagoIO");
